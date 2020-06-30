9d6356330cce9eb33607e67924c89eefc049a007
theodesp <theo.despoudis@wpengine.com>
2020-06-30 20:45:43 +0100

    Ch1 1.1.13

--- site/Chap01/code/Ex1111.java
+++ site/Chap01/code/Ex1111.java
@@ -22,5 +22,12 @@ numbers.
     public static void main(String[] args) {
         boolean[][] arr = {{true, false}, {false, true}, {true, true}};
         printContents(arr);
+        int[] a = new int[10];
+        for (int i = 0; i < 10; i++)
+            a[i] = 9 - i;
+        for (int i = 0; i < 10; i++)
+            a[i] = a[a[i]];
+        for (int i = 0; i < 10; i++)
+            System.out.println(i);
     }
 }
