1945d17f326a2a430138b97c845515de5f011f87
theodesp <theo.despoudis@wpengine.com>
2020-07-06 20:24:27 +0000

    Deployed 1f16ecc with MkDocs version: 1.1.2

--- Chap01/code/Ex126.java
+++ Chap01/code/Ex126.java
@@ -16,10 +16,20 @@ public class Ex126 {
         return result;
     }
 
+    public static String mystery(String s)
+    {
+        int N = s.length();
+        if (N <= 1) return s;
+        String a = s.substring(0, N/2);
+        String b = s.substring(N/2, N);
+        return mystery(b) + mystery(a);
+    }
+
     public static void main(String[] args) {
         String a = "ACTGACG";
         String b = "TGACGAC";
 
         System.out.println(isCircular(a, b));
+        System.out.println(mystery("ABCDEFG"));
     }
 }
