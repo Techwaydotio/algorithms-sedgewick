af2b35554b5522f75431a132a75eb7b006797e59
theodesp <theo.despoudis@wpengine.com>
2020-07-23 20:50:34 +0100

    Ex: 1.3.7

---
+++ docs/Chap01/code/Ex137.java
@@ -0,0 +1,64 @@
+package Chap01.code;
+
+import java.util.Iterator;
+
+public class Ex137 {
+
+    public static class ResizingArrayStack<Item> implements Iterable<Item> {
+        private Item[] a = (Item[]) new Object[1]; // stack items
+        private int N = 0; // number of items
+
+        public boolean isEmpty() {
+            return N == 0;
+        }
+
+        public int size() {
+            return N;
+        }
+
+        public Item peek() {
+            if (N > 0) {
+                return a[0];
+            }
+            return null;
+        }
+
+        private void resize(int max) { // Move stack to a new array of size max.
+            Item[] temp = (Item[]) new Object[max];
+            for (int i = 0; i < N; i++)
+                temp[i] = a[i];
+            a = temp;
+        }
+
+        public void push(Item item) { // Add item to top of stack.
+            if (N == a.length) resize(2 * a.length);
+            a[N++] = item;
+        }
+
+        public Item pop() { // Remove item from top of stack.
+            Item item = a[--N];
+            a[N] = null; // Avoid loitering (see text).
+            if (N > 0 && N == a.length / 4) resize(a.length / 2);
+            return item;
+        }
+
+        public Iterator<Item> iterator() {
+            return new ReverseArrayIterator();
+        }
+
+        private class ReverseArrayIterator implements Iterator<Item> { // Support LIFO iteration.
+            private int i = N;
+
+            public boolean hasNext() {
+                return i > 0;
+            }
+
+            public Item next() {
+                return a[--i];
+            }
+
+            public void remove() {
+            }
+        }
+    }
+}
--- site/Chap01/code/Ex134.java
+++ site/Chap01/code/Ex134.java
@@ -1,45 +1,65 @@
 package Chap01.code;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
 import java.util.Stack;
 
 public class Ex134 {
-    static boolean areBalanced(String expr) {
-        Stack<Character> stack = new Stack<Character>();
+//    static boolean areBalanced(String expr) {
+//        Stack<Character> stack = new Stack<Character>();
+//
+//        // Traversing the Expression
+//        for (int i = 0; i < expr.length(); i++) {
+//            char x = expr.charAt(i);
+//
+//            if (x == '(' || x == '[' || x == '{') {
+//                // Push the element in the stack
+//                stack.push(x);
+//                continue;
+//            }
+//
+//            if (stack.isEmpty())
+//                return false;
+//
+//            switch (x) {
+//                case ')':
+//                    stack.pop();
+//                    if (x != '(')
+//                        return false;
+//                    break;
+//
+//                case '}':
+//                    stack.pop();
+//                    if (x != '{')
+//                        return false;
+//                    break;
+//
+//                case ']':
+//                    stack.pop();
+//                    if (x != '[')
+//                        return false;
+//                    break;
+//            }
+//        }
+//
+//        return (stack.isEmpty());
+//    }
 
-        // Traversing the Expression
-        for (int i = 0; i < expr.length(); i++) {
-            char x = expr.charAt(i);
+    public static void main(String[] args) {
+        Stack<String> stack = new Stack<String>();
+        Deque<String> q = new ArrayDeque<String>();
+        q.offer("Hello");
+        q.offer("World");
+        q.offer("This");
+        q.offer("Is");
+        q.offer("It");
+        while (!q.isEmpty())
+            stack.push(q.poll()); // reverses order of strings
+        while (!stack.isEmpty())
+            q.offer(stack.pop()); // put back in reverse order
 
-            if (x == '(' || x == '[' || x == '{') {
-                // Push the element in the stack
-                stack.push(x);
-                continue;
-            }
-
-            if (stack.isEmpty())
-                return false;
-
-            switch (x) {
-                case ')':
-                    stack.pop();
-                    if (x != '(')
-                        return false;
-                    break;
-
-                case '}':
-                    stack.pop();
-                    if (x != '{')
-                        return false;
-                    break;
-
-                case ']':
-                    stack.pop();
-                    if (x != '[')
-                        return false;
-                    break;
-            }
-        }
-
-        return (stack.isEmpty());
+        while (!q.isEmpty())
+            System.out.println(q.poll());
     }
 }
+
